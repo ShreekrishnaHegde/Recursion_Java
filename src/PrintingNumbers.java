8716dd6e3f422a6d6c382e68d49bc53ceef7f44b
Shreekrishna <hegdeshreekrishna469@gmail.com>
2024-10-01 14:52:28 +0530

    printing numbers in reverse

--- src/PrintingNumbers.java
+++ src/PrintingNumbers.java
@@ -1,13 +1,22 @@
 public class PrintingNumbers {
     //given n=5, return all the integers starting fom 5 till 1.
     public static void main(String[] args) {
-        display(5);
+        display2(5);
     }
-    static void display(int n){
+    static void display1(int n){
         if(n==0)
             return;
         System.out.println(n);
-        display(n-1);
+        display1(n-1);
     }
+
+    static void display2(int n){
+        if(n==0)
+            return;
+        display2(n-1);
+        System.out.println(n);
+    }
+
+
 
 }
