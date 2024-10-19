27cc6f88f93617a8d874e99f4e4c2b30d146ca2e
Shreekrishna <hegdeshreekrishna469@gmail.com>
2024-10-19 22:14:39 +0530

    Comments added

--- src/Basics.java
+++ src/Basics.java
@@ -15,19 +15,30 @@ public class Basics {
     }
 }
 /*
-->Recursion is function calling itself.
+--------------Definition-------------------
 ->The process in which a function calls itself directly or indirectly is called recursion
     and the corresponding function is called a recursive function.
+->Base Case is defined as the condition in Recursive Function, which tells the function when to stop.
+->Tail recursion is defined by having the recursive call as the last operation in the function before returning
+    While non-tail recursion involves other operations after the recursive call
+-.
+------------Working Of Recursion-------------
+->The idea is to represent a problem in terms of one or more smaller sub-problems and
+    add base conditions that stop the recursion.
+
 ---------------Memory Allocation---------------
 ->When any function is called from main(), the memory is allocated to it on the stack.
     A recursive function calls itself, the memory for a called function is allocated on top of memory allocated
     to the calling function and a different copy of local variables is created for each function call.
     When the base case is reached, the function returns its value to the function by whom it is called
     and memory is de-allocated and the process continues.
-->Recursion problems can be solved using iteration and vice versa.
 ->Every function call will take some memory in stack memory.
 ->When there is no base condition in the recursive function, when the memory exceeds the limit,it will give
     stack overflow error.
+
+
+----------------Other Points---------------
+->Recursion problems can be solved using iteration and vice versa.
 ->Space complexity is not constant.
 ->You can convert recursion into iteration and vice versa.
 ->Recursion tree.
@@ -38,8 +49,6 @@ public class Basics {
 ->Recursion Tree
 ->Direct and Indirect recursion
 -Tail and non-tail Recursive
-->A recursive function is tail recursive when a recursive call is the last thing executed by the function.
-
  */
 
 
