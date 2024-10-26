564adf91c4be1df03e516eaa60ad58ea848a935e
Shreekrishna <hegdeshreekrishna469@gmail.com>
2024-10-26 16:01:24 +0530

    Maze with obstacles gave an error

--- src/BackTracking/MazeWithObstacles.java
+++ src/BackTracking/MazeWithObstacles.java
@@ -1,7 +1,35 @@
 package BackTracking;
+/*
+Maze with obstacles
+When you land on a new cell check whether it is river or not.
+if you land on river stop the recursion call
+this is not backtracking
+This is stopping the recursion at some point.
 
-import java.util.Arrays;
-
+ */
 public class MazeWithObstacles {
+    public static void main(String[] args) {
+        boolean board[][]={
+                {true,true,true},
+                {true,false,true},
+                {true,true,true}
+        };
+        pathObstacle("",board,0,0);
+    }
+    static void pathObstacle(String p,boolean[][] maze,int r,int c){
+        if (r== maze.length-1 && c==maze[0].length- 1){
+            System.out.println(p);
+            return;
+        }
+        if(!maze[r][c])
+            return;
+        if (r< maze.length-1){
+            pathObstacle(p+'D',maze,r+1,c);
+        }
+        if(c< maze[0].length){
+            pathObstacle(p+'R',maze,r,c+1);
+        }
 
+
+    }
 }
